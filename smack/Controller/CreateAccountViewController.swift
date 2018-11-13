b011cb857330cd8212dbcabcf67e6c78e6c663da
Sean Saoirse <seansaoirse@Seans-MacBook-Pro.local>
2018-11-13 18:48:38 +0700

    Finished createa account frontend

---
+++ smack/Controller/CreateAccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateAccountViewController.swift
+//  smack
+//
+//  Created by Sean Saoirse on 13/11/18.
+//  Copyright © 2018 Sean Saoirse. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
